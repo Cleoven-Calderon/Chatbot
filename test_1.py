9d2e1c9c42affed0541a87711a7933aa9b011879
Calderon CJ <calderoncj58@gmail.com>
2025-09-09 21:06:25 +0800

    Initial commit

--- test_1.py
+++ test_1.py
@@ -1,24 +1,17 @@
-import gradio as gr  # Frontend module
+import gradio as gr
 import ollama
-import warnings
-warnings.filterwarnings("ignore")
 
-MODEL = "qwen2.5-coder:3b"  # Example Model
-
-""" Download Ollama models on the official Ollama site.
-    Current available: *** """
+MODEL = "qwen2.5-coder:3b"  # Current available models: qwen2.5-coder:3b, llama3.2:1b
 
 simple_css = """
-    .gradio-container {
+
+.gradio-container {
     background: linear-gradient(135deg, #667eea 0%, #764ba2 100%) !important;
     min-height: 100vh;
     padding: 20px;
-
+   
 }
 """
-
-system_prompt = ("You are a helpful assistant that provides concise, direct answers. "
-                 "Keep responses brief and to the point. Avoid unnecessary explanations and wordiness.")
 
 
 def chat_with_ollama(message, history):
@@ -29,10 +22,10 @@ def chat_with_ollama(message, history):
     for human_msg, ai_msg in history:
         messages.append({"role": "user", "content": human_msg})
         if ai_msg:  # Add AI response if it exists
-            messages.append({"role": "assistant", "content": system_prompt})
+            messages.append({"role": "assistant", "content": ai_msg})
     messages.append({"role": "user", "content": message})
 
-    # Stream the response, this creates a 'typing' effect to the Ai response
+    # Stream the response
     partial_response = ""
     stream = ollama.chat(
         model=MODEL,
@@ -53,8 +46,7 @@ demo = gr.ChatInterface(
     chat_with_ollama,
     title="Qwen-Codify 2.5",
     description="Local Ai assistant feat. qwen2.5",
-    theme="soft",  # Change to 'default' for more control
-    type="tuples",
+    theme="soft",
     css=simple_css,
     examples=[["Hello!"], ["How LLMs work?"], ["Need help with Python code."]],
     example_labels=[
@@ -63,13 +55,13 @@ demo = gr.ChatInterface(
         "🐍 Help me program."],
 
     chatbot=gr.Chatbot(
-        height=500,
-        type="tuples",
-        avatar_images=(
-            "llama logo.png",
-            "qwen.png"
+            height=500,
+            avatar_images=(
+                "llama logo.png",
+                "qwen.png"
+            )
         )
-    )
 )
 
-demo.launch(share=False)  # share=True, to publicly share link, otherwise local
+
+demo.launch(share=False)
